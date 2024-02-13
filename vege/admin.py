b84f29e610caacd2fc351b26c3b8d71657b7ea8e
Bhavya <bhogathibhavya99@gmail.com>
2024-02-13 16:49:45 +0530

    Learnt about ORM and its working.

--- vege/admin.py
+++ vege/admin.py
@@ -5,3 +5,8 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Receipe)
+
+admin.site.register(Department)
+admin.site.register(StudentID)
+admin.site.register(Student)
+
